e44f9e928f2da16c594db5af8b95bc2fc68c630b
todd-estes <elestes@gmail.com>
2024-10-21 20:04:17 -0400

    feat: Convert csv to dataframe; convert Start Time column to datetime; add Month and Day of Week columns

--- bikeshare.py
+++ bikeshare.py
@@ -46,7 +46,7 @@ def get_filters():
     return city, month, day
 
 
-# def load_data(city, month, day):
+def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
 
@@ -57,8 +57,11 @@ def get_filters():
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-
-
+    df = pd.read_csv(f'{city}.csv')
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+    df['Month'] = df['Start Time'].dt.month
+    df['Day of Week'] = df['Start Time'].dt.day_name()
+    
     return df
 
 
@@ -139,11 +142,11 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        print(df.info())
+        # time_stats(df)
+        # station_stats(df)
+        # trip_duration_stats(df)
+        # user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
